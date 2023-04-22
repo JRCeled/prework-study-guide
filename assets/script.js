4b4e74d3f8c92d9613aec08e5746e4c44c5ebc2a
JRCeled <jesusrceledon@gmail.com>
2023-04-22 17:49:03 -0500

    updated

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,17 @@ function listTopics() {
 }
 
 function selectTopic() {
- if (topic === 'HTML') {
-   console.log("Let's study HTML!");
- } else if (topic === 'CSS') {
-   console.log("Let's study CSS!");
- } else if (topic === 'Git') {
-   console.log("Let's study Git!");
- } else if (topic === 'JavaScript') {
-   console.log("Let's study JavaScript!");
- } else {
-   console.log('Please try again!');
- }
-}
+    if (topics === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topics === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topics === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topics === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
 listTopics()
 selectTopic()
